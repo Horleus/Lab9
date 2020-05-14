4c3ce81f4f40cda8a89c1e7ffe7c25d0a7381d82
f5 <f5@f5-X51RL>
2020-05-14 20:03:31 +0300

    Вказанні змінні

--- src/Lab9.cpp
+++ src/Lab9.cpp
@@ -8,7 +8,21 @@ int main()
 {
    double Ax,Ay,Bx,By,Cx,Cy,Dx,Dy;
 
+   cout << "A(x,y)";
+   cin >> Ax >> Ay;
+   cout << endl;
 
+   cout << "B(x,y)";
+      cin >> Bx >> By;
+      cout << endl;
+
+   cout << "C(x,y)";
+      cin >> Cx >> Cy;
+      cout << endl;
+
+      cout << "D(x,y)";
+         cin >> Dx >> Dy;
+         cout << endl;
 
 
 	return 0;
